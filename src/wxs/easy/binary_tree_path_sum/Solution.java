878fa6498c9fa6c1390ecc311077681a7d7f347d
xinyongwxs <xinyongwxs@163.com>
2017-06-29 00:30:27 +0800

    Update sum function.

--- src/wxs/easy/binary_tree_path_sum/Solution.java
+++ src/wxs/easy/binary_tree_path_sum/Solution.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-	protected boolean currentSum(TreeNode node, 
+	protected boolean currentSum(TreeNode node,
+			List<List<Integer>> pathList,
 			List<Integer> path, 
 			int currentSum, 
 			int target) {
@@ -13,15 +14,16 @@ public class Solution {
 		currentSum += node.getVal();
 		if (currentSum == target) {
 			if (node.getLeft() == null && node.getRight() == null) {
+				pathList.add(path);
 				isRightPath = true;
 			} else {
 				isRightPath = false;
 			}
 		} else if (currentSum < target) {
 			if (node.getLeft() != null) {
-				currentSum(node.getLeft(), path, currentSum, target);
+				currentSum(node.getLeft(), pathList, path, currentSum, target);
 			} else if (node.getRight() != null) {
-				currentSum(node.getRight(), path, currentSum, target);
+				currentSum(node.getRight(), pathList, path, currentSum, target);
 			}
 		} else {
 			return false;
@@ -33,7 +35,9 @@ public class Solution {
 	public List<List<Integer>> binaryTreePathSum(TreeNode root, int target) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		List<Integer> path = new ArrayList<Integer>();
-		
+		path.add(root.getVal());
+		Integer currentSum = root.getVal();
+		this.currentSum(root, result, path, currentSum, target);
 		return null;
 	}
 }
